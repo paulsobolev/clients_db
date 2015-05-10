e17706868fbbcc33a5d1eaa0ba9cce54f23aac24
Paul Sobolev <cloud@paulsobolev.pro>
2015-05-11 01:51:27 +0500

    create Application
    create application, router and controller (showUsers)

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+(function(window, $, Backbone) {
+
+    'use strict';
+
+    window.DBApp = new Backbone.Marionette.Application();
+
+    var DBAppRouter = Backbone.Marionette.AppRouter.extend({
+        routes: {
+            "": "showUsers"
+        },
+        showUsers: function() {
+            // create search model
+            // create search view
+            // create collection
+            // create collection view
+            // get data (ajax)
+        }
+    });
+
+    DBApp.on('start', function() {
+        new DBAppRouter();
+        Backbone.history.start();
+    });
+
+})(window, jQuery, Backbone);
